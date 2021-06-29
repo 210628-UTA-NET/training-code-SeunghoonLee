c1acc398579c0c9a0a8eb80cec95ad3375b229a1
Seunghoon Lee <lsh2002@hotmail.com>
2021-06-28 21:33:43 -0400

    Added House class

---
+++ CoreC#/HelloWorld/House.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace HouseFunction
+{
+    public class House
+    {
+        private string _color;
+        private int _price;
+
+        public House()
+        {
+            Console.WriteLine("That constructor of this class was used");
+            _color = "Red";
+        }
+
+        public int Price
+        {
+            get
+            {
+                return _price;
+            }
+
+            set
+            {
+                _price = value;
+            }
+        }
+
+        public string Name { get; set; }
+
+        public string getColor()
+        {
+            return _color;
+        }
+
+        public void setColor(string p_color)
+        {
+            _color = p_color;
+        }
+
+
+    }
+}
