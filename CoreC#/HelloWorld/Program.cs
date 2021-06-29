7dcb1f2146b6484320dc18ca2cc8dbf6d75fdfe3
Seunghoon Lee <lsh2002@hotmail.com>
2021-06-28 21:44:39 -0400

    Modified Program.cs

--- CoreC#/HelloWorld/Program.cs
+++ CoreC#/HelloWorld/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using HouseFunction;
 
 namespace HelloWorld
 {
     class Program
     {
+        private static string _first = "Hello";
+        private static string _last = "World!";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Whatever I want");
+
+            Test obj = new Test();
+            obj.SomeMethod();
+
+            Console.WriteLine(_first + _last);
+            Console.WriteLine(obj.SomeMethod());
+
+            House objHouse = new House();
+            Console.WriteLine(objHouse.getColor());
+            objHouse.setColor("Blue");
+            Console.WriteLine(objHouse.getColor());
+            objHouse.Price = 8000;
+            Console.WriteLine(objHouse.Price);
+            objHouse.Name = "Seunghoon's House";
+            Console.WriteLine(objHouse.Name);
+
+        }
+    }
+
+    class Test
+    {
+        public string SomeMethod()
+        {
+            return "Hello World SomeMethod";
         }
     }
 }
