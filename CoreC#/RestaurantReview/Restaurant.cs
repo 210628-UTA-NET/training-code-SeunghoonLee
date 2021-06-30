838c3a4c98fbd0c1a3241e0a330432622c35eeaa
Seunghoon Lee <lsh2002@hotmail.com>
2021-06-30 15:29:00 -0400

    Added Restaurant

---
+++ CoreC#/RestaurantReview/Restaurant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RestaurantReview
+{
+    public class Restaurant
+    {
+        private string _city;
+        public Restaurant()
+        {}
+        public string Name { get; set; }
+        public string City { 
+            get; 
+            set
+            {
+                if(!Regex.IsMatch(value, "^[A-Za-z .]+$"))
+                {
+                    throw new Exception("City can only hold letters");
+                }
+                _city = value;
+            } 
+        }
+        public string State { get; set; }
+    }
+}
