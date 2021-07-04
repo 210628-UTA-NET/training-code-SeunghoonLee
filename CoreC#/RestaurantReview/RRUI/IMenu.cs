783476df10889842c4e3b1fab6e495f848b524e1
Seunghoon Lee <lsh2002@hotmail.com>
2021-07-04 03:39:29 -0400

    Added IMenu

---
+++ CoreC#/RestaurantReview/RRUI/IMenu.cs
@@ -0,0 +1,16 @@
+namespace RRUI
+{
+    public interface IMenu
+    {
+        /// <summary>
+        /// Will display the overall menu of the class and the choices you can take in that menu class
+        /// </summary>
+        void Menu();
+
+        /// <summary>
+        /// This method will record the user's choice and change your many based on their input
+        /// </summary>
+        /// <returns>a value that will dictate what menu to change to</returns>
+        string YourChoice();
+    }
+}
