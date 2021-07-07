95934fa0aeed9a7135cb57f64e0dbd4e71d9984d
Seunghoon Lee <lsh2002@hotmail.com>
2021-07-07 07:42:16 -0400

    Added Factory Pattern

---
+++ CoreC#/RestaurantReview/RRUI/MenuFactory.cs
@@ -0,0 +1,22 @@
+using RRBL;
+using RRDL;
+
+namespace RRUI {
+    public class MenuFactory : IFactory
+    {
+        public IMenu GetMenu(MenuType p_menu)
+        {
+            switch(p_menu)
+                {
+                    case MenuType.MainMenu :
+                        return new MainMenu();
+                    case MenuType.RestaurantMenu :
+                        return new RestaurantMenu();
+                    case MenuType.ShowRestaurantMenu :
+                        return new ShowRestaurantMenu(new RestaurantBL(new Repository()));
+                    default :
+                        return null;
+                }
+
+        }
+    }
