928ddeef2c98b09da18eeb9053b9255d77dd8411
Seunghoon Lee <lsh2002@hotmail.com>
2021-07-08 23:26:16 -0400

    Added Entities using dotnet-ef

---
+++ CoreC#/RestaurantReview/RRDL/Entities/LCGDBContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace RRDL.Entities
+{
+    public partial class LCGDBContext : DbContext
+    {
+        public LCGDBContext()
+        {
+        }
+
+        public LCGDBContext(DbContextOptions<LCGDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Restaurant> Restaurants { get; set; }
+        public virtual DbSet<Review> Reviews { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Restaurant>(entity =>
+            {
+                entity.ToTable("Restaurant");
+
+                entity.Property(e => e.City)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.State)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Review>(entity =>
+            {
+                entity.ToTable("Review");
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Rating).HasColumnType("decimal(10, 2)");
+
+                entity.HasOne(d => d.Restaurant)
+                    .WithMany(p => p.Reviews)
+                    .HasForeignKey(d => d.RestaurantId)
+                    .HasConstraintName("fk_review_restaurant");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
